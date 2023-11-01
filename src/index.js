fc373d471f674083ff443a32988438c07812f465
Abhinav Meda <abhinavmeda@Abhinavs-Air.phub.net.cable.rogers.com>
2023-11-01 11:42:33 -0400

    small modification

--- src/index.js
+++ src/index.js
@@ -8,11 +8,41 @@ const thingy  = async () => {
         const githubToken = core.getInput('repo-token', {required: true})
         const octokit = github.getOctokit(githubToken);
         
-        const query =  `query {
-                            viewer {
-                                login
+        const query =  `
+        query GetEachRowInProjectTabel {
+            node(id: "PVT_kwHOCESN7M4AW9ai") {
+              ... on ProjectV2 {
+                items(first: 10) {
+                  nodes {
+                    id
+                              __typename
+                    fieldValues(first: 10){
+                      __typename
+                      # edges{
+                      #   node{
+                      #     __typename
+                      #   }
+                      # }
+                      rows: nodes{
+                        # __typename
+                        ... on ProjectV2ItemFieldSingleSelectValue{
+                          name
+                          field {
+                            ... on ProjectV2FieldCommon {
+                              name
+                              id
                             }
-                        }`
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        
+        `
         
         const user = await octokit.graphql(query)
         const contextPayload = github.context.payload;
