fd381f98c4b14cb194cdcc345bbff0e947b04260
Abhinav Meda <abhinavmeda@Abhinavs-Air.phub.net.cable.rogers.com>
2023-11-01 11:04:53 -0400

    trying to just get node id, owner, and repo name

--- src/index.js
+++ src/index.js
@@ -5,15 +5,24 @@ const github = require('@actions/github');
 try{
 
     // these variables are set using the inputs defined in action.yml
-    const githubToken = core.getInput('repo-token', {required: true})
+const githubToken = core.getInput('repo-token', {required: true})
 
 // the object which will interact with GitHub APIs
 const octokit = github.getOctokit(githubToken);
 
 // this will contain information related to the issue that was just opened
 const contextPayload = github.context.payload;
-
-console.log(JSON.stringify(octokit))
+let user
+try{
+    user = await octokit.request('GET /user', {
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      })
+}catch(error){
+    console.log(error)
+}
+console.log(JSON.stringify(user))
 console.log(JSON.stringify(contextPayload))
 
 }catch (error) {
