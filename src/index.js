a5f51cabcd3b651581526113c3a20bf883932d95
Abhinav Meda <abhinavmeda@Abhinavs-Air.phub.net.cable.rogers.com>
2023-11-02 12:54:38 -0400

    small modification

--- src/index.js
+++ src/index.js
@@ -9,36 +9,9 @@ const thingy  = async () => {
         const octokit = github.getOctokit(githubToken);
         
         const query =  `
-        query GetEachRowInProjectTabel {
-            node(id: "PVT_kwHOCESN7M4AW9ai") {
-              ... on ProjectV2 {
-                items(first: 10) {
-                  nodes {
-                    id
-                              __typename
-                    fieldValues(first: 10){
-                      __typename
-                      # edges{
-                      #   node{
-                      #     __typename
-                      #   }
-                      # }
-                      rows: nodes{
-                        # __typename
-                        ... on ProjectV2ItemFieldSingleSelectValue{
-                          name
-                          field {
-                            ... on ProjectV2FieldCommon {
-                              name
-                              id
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
+        query {
+            viewer {
+              login
             }
           }
         
