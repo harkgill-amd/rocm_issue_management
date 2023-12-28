a474c2a3f1d17f6a34b90d23c1ec3c9af576cde7
Meda, Abhinav <Abhinav.Meda@amd.com>
2023-12-28 11:03:55 -0500

    small change

--- src/index.js
+++ src/index.js
@@ -127,7 +127,7 @@ function hasInstinct(gpuList){
   console.log(gpuList)
   gpuList.array.forEach(element => {
     if (element.includes("Instinct")){
-      return true
+      return true;
     }
   });
   return false
@@ -147,8 +147,8 @@ async function run() {
         const num = contextPayload.issue.number
         const title = "TESTING-AMD-GITHUB-RUNNER"
         // console.log("JSON contextPayload.issue:  ",JSON.stringify(contextPayload.issue))
-        let [gpu, rocmVersions] = await extractInfo(octokit, body, num)
-        let gpuLabel = hasInstinct(gpu) === true ? "Instinct" : "Radeon"
+        let [gpu, rocmVersions] = await extractInfo(octokit, body, num);
+        let gpuLabel = hasInstinct(gpu) === true ? "Instinct" : "Radeon";
         let rocmLabel = rocmVersions.sort()
         rocmLabel = rocmLabel[rocmLabel.length - 1].replace(" ", "_")
 
